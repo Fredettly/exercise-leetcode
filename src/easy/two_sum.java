5c616da816c35374396142a108b22027148a4b6e
Fredettly <794760487@qq.com>
2020-02-23 21:48:46 +0800

    climb stairs

--- src/easy/two_sum.java
+++ src/easy/two_sum.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
  */
 public class two_sum {
 
-    //     static int[] twoSum(int[] nums, int target) {
+//    static int[] twoSum(int[] nums, int target) {
 //        for (int i = 0; i < nums.length; i++) {
 //            for (int j = i + 1; j < nums.length; j++) {
 //                if (nums[j] == target - nums[i]) {
-//                    return new int[] { i, j };
+//                    return new int[]{i, j};
 //                }
 //            }
 //        }
