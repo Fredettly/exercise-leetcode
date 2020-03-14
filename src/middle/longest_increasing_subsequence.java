e38f64bab6a1a7136fdb163e9539fd2164b33575
Fredettly <794760487@qq.com>
2020-03-14 21:03:47 +0800

    longest-increasing-subsequence

---
+++ src/middle/longest_increasing_subsequence.java
@@ -0,0 +1,34 @@
+package middle;
+
+import java.util.Arrays;
+
+/**
+ * Created by xwx_ on 2020/3/14
+ */
+public class longest_increasing_subsequence {
+    public static int lengthOfLIS(int[] nums) {
+        int len = nums.length;
+        if (len < 2) {
+            return len;
+        }
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+        }
+        int res = dp[0];
+        for (int i = 0; i < len; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 1, 4, 123, 18};
+        System.out.println(lengthOfLIS(nums));
+    }
+}
