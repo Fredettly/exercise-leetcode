65d333b707e9c08238420c8836f7d5f6d854675c
Fredettly <794760487@qq.com>
2020-03-18 21:16:33 +0800

    rectangle-overlap

--- src/easy/two_sum.java
+++ src/easy/two_sum.java
@@ -4,20 +4,36 @@ import java.util.HashMap;
 /**
  * Created by xwx_ on 2020/1/21
  */
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ *      Given nums = [2, 7, 11, 15], target = 9,
+ *
+ *      Because nums[0] + nums[1] = 2 + 7 = 9,
+ *      return [0, 1].
+ */
 public class two_sum {
 
-//    static int[] twoSum(int[] nums, int target) {
-//        for (int i = 0; i < nums.length; i++) {
-//            for (int j = i + 1; j < nums.length; j++) {
-//                if (nums[j] == target - nums[i]) {
-//                    return new int[]{i, j};
-//                }
-//            }
-//        }
-//        throw new RuntimeException("No two sum solution");
-//    }
+/*
+    int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new RuntimeException("No two sum solution");
+    }
+*/
 
-    public static int[] twoSum(int[] nums, int target) {
+
+    public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
@@ -31,7 +47,9 @@ public class two_sum {
 
     public static void main(String[] args) {
         int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        System.out.println(Arrays.toString(twoSum(nums, target)));
+        int target = 26;
+        two_sum twoSum = new two_sum();
+        int[] result = twoSum.twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
     }
 }
