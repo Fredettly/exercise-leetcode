00c22a0d5ee8b3247f954b833c7373c18a22e9b6
Fredettly <794760487@qq.com>
2020-03-23 21:14:15 +0800

    middle-of-the-linked-list

---
+++ src/easy/middle_of_the_linked_list.java
@@ -0,0 +1,115 @@
+import java.io.IOException;
+
+/**
+ * Created by xwx_ on 2020/3/23
+ */
+
+/**
+ * Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+ *
+ * If there are two middle nodes, return the second middle node.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,4,5]
+ * Output: Node 3 from this list (Serialization: [3,4,5])
+ * The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+ * Note that we returned a ListNode object ans, such that:
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+ *
+ * Example 2:
+ *
+ * Input: [1,2,3,4,5,6]
+ * Output: Node 4 from this list (Serialization: [4,5,6])
+ * Since the list has two middle nodes with values 3 and 4, we return the second one.
+ *
+ *
+ *
+ * Note:
+ *
+ *     The number of nodes in the given list will be between 1 and 100.
+ */
+
+
+public class middle_of_the_linked_list {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode p = head, q = head;
+        while (p != null && p.next != null) {
+            p = p.next.next;
+            q = q.next;
+        }
+        return q;
+    }
+
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for (int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//        String line;
+//        while ((line = in.readLine()) != null) {
+        String line = "[3,4,6,2,7,9,3,6,1,3]";
+
+        ListNode head = stringToListNode(line);
+
+        ListNode ret = new middle_of_the_linked_list().middleNode(head);
+
+        String out = listNodeToString(ret);
+
+        System.out.print(out);
+//        }
+    }
+}
+
---
+++ src/test/QuickSearch.java
@@ -0,0 +1,38 @@
+package test;
+
+import java.util.Arrays;
+
+/**
+ * Created by xwx_ on 2020/3/22
+ */
+public class QuickSearch {
+    void quick_sort(int s[], int l, int r) {
+        if (l < r) {
+            //Swap(s[l], s[(l + r) / 2]); //将中间的这个数和第一个数交换
+            int i = l, j = r, x = s[l];
+            while (i < j) {
+                while (i < j && s[j] >= x) // 从右向左找第一个小于x的数
+                    j--;
+                if (i < j)
+                    s[i++] = s[j];
+
+                while (i < j && s[i] < x) // 从左向右找第一个大于等于x的数
+                    i++;
+                if (i < j)
+                    s[j--] = s[i];
+            }
+            s[i] = x;
+            quick_sort(s, l, i - 1); // 递归调用
+            quick_sort(s, i + 1, r);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] s = {4, 5, 6, 3, 2, 1};
+        int l = 0;
+        int r = 5;
+        QuickSearch quickSearch = new QuickSearch();
+        quickSearch.quick_sort(s, l, r);
+        System.out.println(Arrays.toString(s));
+    }
+}
